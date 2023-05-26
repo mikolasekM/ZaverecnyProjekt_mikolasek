5cad7e4a77ed71da817ac23220ff9e1f6f154263
mikolasekM <125461226+mikolasekM@users.noreply.github.com>
2023-05-26 12:22:52 +0200

    3

--- ZaverecnyProjekt mikolasek/Program.cs
+++ ZaverecnyProjekt mikolasek/Program.cs
@@ -17,12 +17,105 @@ class zaverecnyProjekt
                 AnsiConsole.Markup($"moje skore:[green]{mySkore}[/].enemy skore:[red]{enemySkore}[/]");
                 Console.WriteLine("zadej pouze K(kamen) N(nuzky) P(papir)");
                 choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "K" or "N" or "P": break;
+                    default:
+                        {
+                            Console.WriteLine("nevybral jsi spravny objekt");
+                            Console.ReadKey();
+                            continue;
+                        }
+                }
+                int enemychoice = random.Next(0, 3);
+                if (enemychoice == 0)
+                {
+                    Console.WriteLine(" enemy vybral kamínek");
+                    switch (choice)
+                    {
+                        case "K":
+                            Console.WriteLine("remízka");
+                            mySkore++;
+                            enemySkore++;
+                            break;
+                        case "N":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("prohrál jsi nůbe");
+                            enemySkore++;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case "P":
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("vyhrál jsi :D");
+                            mySkore++;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                }
+                else if (enemychoice == 1)
+                {
+                    Console.WriteLine("enemy vybral nůžtičky");
+                    switch (choice)
+                    {
+                        case "K":
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("vyhral jsi :D");
+                            mySkore++;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case "N":
+                            Console.WriteLine("remízka");
+                            mySkore++;
+                            enemySkore++;
+                            break;
+                        case "P":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("prohrál jsi nůbe");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            enemySkore++;
+                            break;
+                    }
+                }
+                else if (enemychoice == 2)
+                {
+                    Console.WriteLine("enemy použil papír");
+                    switch (choice)
+                    {
+                        case "K":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("prohrál jsi nůbe");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            enemySkore++;
+                            break;
+                        case "N":
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("vyhrál jsi :D");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            mySkore++;
+                            break;
+                        case "P":
+                            Console.WriteLine("remízka");
+                            mySkore++;
+                            enemySkore++;
+                            break;
+                    }
+                }
+                Console.ReadKey();
             }
-            int enemymoznost = random.Next(0, 3);
-            string[] counterList;
-            if (counterList.Contains (choice)) { }
-            if(counterList.Contains(choice))
-            if(counterList.Contains(choice))
+               if (enemySkore== 5)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("LOOSER JSII ;-;");
+                Console.ForegroundColor = ConsoleColor.White;
+            }      
+               else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("JSI WINNER °-°");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
         catch(Exception)
         {
